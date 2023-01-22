cfc84c467b108147ce8b8dae314cf9278733fa33
dflorencia <zS16025010@estudiantes.uv.mx>
2023-01-22 11:02:33 -0600

    primero

---
+++ promedio/Program.cs
@@ -0,0 +1,21 @@
+﻿String numero;
+double primeraNota;
+double segundaNota;
+double terceraNota;
+double cuartaNota;
+
+Console.Write("Primera nota: ");
+numero = Console.ReadLine();
+primeraNota = Convert.ToDouble(numero);
+Console.Write("Segunda nota: ");
+numero = Console.ReadLine();
+segundaNota = Convert.ToDouble(numero);
+Console.Write("Trecera nota: ");
+numero = Console.ReadLine();
+terceraNota = Convert.ToDouble(numero);
+Console.Write("cuarta nota: ");
+numero = Console.ReadLine();
+cuartaNota = Convert.ToDouble(numero);
+
+double promedio = (primeraNota + segundaNota + terceraNota + cuartaNota) / 4;
+Console.Write("Su promedio fue "+ promedio);
