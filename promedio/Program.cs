8d76377a2722ce1c9c76c389b17e37ee8835da7e
joseflodan <joseflodan@gmail.com>
2023-01-22 11:56:49 -0600

    segundo

--- promedio/Program.cs
+++ promedio/Program.cs
@@ -1,21 +1,18 @@
 ﻿String numero;
-double primeraNota;
-double segundaNota;
-double terceraNota;
-double cuartaNota;
+double[] notas = new double[4];
 
 Console.Write("Primera nota: ");
 numero = Console.ReadLine();
-primeraNota = Convert.ToDouble(numero);
+notas[0] = Convert.ToDouble(numero);
 Console.Write("Segunda nota: ");
 numero = Console.ReadLine();
-segundaNota = Convert.ToDouble(numero);
+notas[1] = Convert.ToDouble(numero);
 Console.Write("Trecera nota: ");
 numero = Console.ReadLine();
-terceraNota = Convert.ToDouble(numero);
+notas[2] = Convert.ToDouble(numero);
 Console.Write("cuarta nota: ");
 numero = Console.ReadLine();
-cuartaNota = Convert.ToDouble(numero);
+notas[3] = Convert.ToDouble(numero);
 
-double promedio = (primeraNota + segundaNota + terceraNota + cuartaNota) / 4;
+double promedio = notas.Sum() / notas.Length;
 Console.Write("Su promedio fue "+ promedio);
